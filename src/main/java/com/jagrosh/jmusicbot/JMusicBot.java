0f42b476a064be583ad5d55194a10cd7161e01a6
Lillie <austins355@yahoo.com>
2024-03-11 07:43:18 -0500

    Reverse changes from PR 1486 in upstream
    We should be allowed to run the bot in "unsupported manners" if we wish, just tell us that it might break things instead of breaking it intentionally. Especially don't exit the entire bot after connecting it to Discord... That causes obvious problems if it's set up as a systemd service. Anyone can see that.

--- src/main/java/com/jagrosh/jmusicbot/JMusicBot.java
+++ src/main/java/com/jagrosh/jmusicbot/JMusicBot.java
@@ -126,10 +126,7 @@ public class JMusicBot
             String unsupportedReason = OtherUtil.getUnsupportedBotReason(jda);
             if (unsupportedReason != null)
             {
-                prompt.alert(Prompt.Level.ERROR, "JMusicBot", "JMusicBot cannot be run on this Discord bot: " + unsupportedReason);
-                try{ Thread.sleep(5000);}catch(InterruptedException ignored){} // this is awful but until we have a better way...
-                jda.shutdown();
-                System.exit(1);
+                prompt.alert(Prompt.Level.ERROR, "JMusicBot", "Unsupported behavior detected: " + unsupportedReason);
             }
             
             // other check that will just be a warning now but may be required in the future
